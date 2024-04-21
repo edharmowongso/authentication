ca1ea1662872057a9caf734eaec2742e772df35c
Eka PD <edharmowongso@gmail.com>
2024-04-21 23:15:38 +0700

    fix: check authorization
    Signed-off-by: Eka PD <edharmowongso@gmail.com>

--- app/middleware/authorization.js
+++ app/middleware/authorization.js
@@ -1,5 +1,19 @@
 'use strict'
 
+const { UnauthorizedError } = require('../errors')
+
 module.exports = (req, res, next) => {
+  const Authorization = req.get('Authorization')
+  let SplitAuthorization = null
+  try {
+    SplitAuthorization = Authorization.split(' ')
+  } catch (err) {
+    return next(new UnauthorizedError('Missing authorization header'))
+  }
+
+  if (SplitAuthorization.length !== 2) {
+    return next(new UnauthorizedError('Invalid header format'))
+  }
+
   next()
 }
